aa1e32542be647e4306ee6bdab89eff4f0285150
x0ret <x0ret@localhost>
2019-07-02 01:34:02 +0430

    Simplify handling mkfunc

--- decompyle3/parsers/parse3.py
+++ decompyle3/parsers/parse3.py
@@ -1211,55 +1211,6 @@ class Python3Parser(PythonParser):
                 )
                 self.add_unique_rule(rule, opname, token.attr, customize)
 
-                if opname.startswith("MAKE_FUNCTION_A"):
-                    rule = (
-                        "mkfunc_annotate ::= %s%sannotate_tuple LOAD_CONST LOAD_CONST %s"
-                        % (
-                            ("pos_arg " * (args_pos)),
-                            ("call " * (annotate_args - 1)),
-                            opname,
-                        )
-                    )
-                    self.add_unique_rule(rule, opname, token.attr, customize)
-                    if annotate_args > 0:
-                        rule = (
-                            "mkfunc_annotate ::= %s%s%sannotate_tuple load_closure LOAD_CODE LOAD_STR %s"
-                            % (
-                                "pos_arg " * args_pos,
-                                kwargs_str,
-                                "annotate_arg " * (annotate_args - 1),
-                                opname,
-                            )
-                        )
-                    else:
-                        rule = "mkfunc ::= %s%s load_closure LOAD_CODE LOAD_STR %s" % (
-                            "pos_arg " * args_pos,
-                            kwargs_str,
-                            opname,
-                        )
-                    self.add_unique_rule(rule, opname, token.attr, customize)
-
-                    # Normally we remove EXTENDED_ARG from the opcodes, but in the case of
-                    # annotated functions can use the EXTENDED_ARG tuple to signal we have an annotated function.
-                    # Yes this is a little hacky
-                    rule = (
-                        "mkfunc_annotate ::= %s%sannotate_tuple LOAD_CONST LOAD_CONST EXTENDED_ARG %s"
-                        % (
-                            ("pos_arg " * (args_pos)),
-                            ("call " * (annotate_args - 1)),
-                            opname,
-                        )
-                    )
-                    self.add_unique_rule(rule, opname, token.attr, customize)
-                    rule = (
-                        "mkfunc_annotate ::= %s%sannotate_tuple LOAD_CONST LOAD_CONST EXTENDED_ARG %s"
-                        % (
-                            ("pos_arg " * (args_pos)),
-                            ("annotate_arg " * (annotate_args - 1)),
-                            opname,
-                        )
-                    )
-                    self.addRule(rule, nop_func)
             elif opname == "RETURN_VALUE_LAMBDA":
                 self.addRule(
                     """
