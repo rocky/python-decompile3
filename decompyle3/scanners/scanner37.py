b58d0204242c55ed77432e22cf3cdaa91b27559c
rocky <rocky@gnu.org>
2019-12-07 17:10:15 -0500

    Remove scanner36.py

--- decompyle3/scanners/scanner37.py
+++ decompyle3/scanners/scanner37.py
@@ -22,7 +22,6 @@ This sets up opcodes Python's 3.7 and calls a generalized
 scanner routine for Python 3.
 """
 
-from decompyle3.scanners.scanner36 import Scanner36
 from decompyle3.scanners.scanner3 import Scanner3
 
 # bytecode verification, verify(), uses JUMP_OPs from here
@@ -32,13 +31,36 @@ from xdis.opcodes import opcode_37 as opc
 JUMP_OPs = opc.JUMP_OPS
 
 
-class Scanner37(Scanner36):
+class Scanner37(Scanner3):
     def __init__(self, show_asm=None):
         Scanner3.__init__(self, 3.7, show_asm)
         return
 
     pass
 
+    def ingest(self, co, classname=None, code_objects={}, show_asm=None) -> tuple:
+        tokens, customize = Scanner3.ingest(self, co, classname, code_objects, show_asm)
+        not_pypy36 = not (self.version == 3.6 and self.is_pypy)
+        for t in tokens:
+            # The lowest bit of flags indicates whether the
+            # var-keyword argument is placed at the top of the stack
+            if not_pypy36 and t.op == self.opc.CALL_FUNCTION_EX and t.attr & 1:
+                t.kind = "CALL_FUNCTION_EX_KW"
+                pass
+            elif t.op == self.opc.BUILD_STRING:
+                t.kind = "BUILD_STRING_%s" % t.attr
+            elif t.op == self.opc.CALL_FUNCTION_KW:
+                t.kind = "CALL_FUNCTION_KW_%s" % t.attr
+            elif t.op == self.opc.FORMAT_VALUE:
+                if t.attr & 0x4:
+                    t.kind = "FORMAT_VALUE_ATTR"
+                    pass
+            elif not_pypy36 and t.op == self.opc.BUILD_MAP_UNPACK_WITH_CALL:
+                t.kind = "BUILD_MAP_UNPACK_WITH_CALL_%d" % t.attr
+            elif not_pypy36 and t.op == self.opc.BUILD_TUPLE_UNPACK_WITH_CALL:
+                t.kind = "BUILD_TUPLE_UNPACK_WITH_CALL_%d" % t.attr
+            pass
+        return tokens, customize
 
 if __name__ == "__main__":
     from decompyle3 import PYTHON_VERSION
