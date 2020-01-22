ae4890b7649a4e3a61c85a505445d19b7f7ad681
rocky <rocky@gnu.org>
2020-01-22 07:12:08 -0500

    Check that the jumps to jump fix jumps to a *jump*

--- decompyle3/scanners/scanner37base.py
+++ decompyle3/scanners/scanner37base.py
@@ -269,7 +269,7 @@ class Scanner37Base(Scanner):
             # "extended arg" backward jump to the same location.
             if inst.opname == "JUMP_FORWARD":
                 jump_inst = self.insts[self.offset2inst_index[inst.argval]]
-                if jump_inst.has_extended_arg:
+                if jump_inst.has_extended_arg and jump_inst.opname.startswith("JUMP"):
                     # Create comination of the jump-to instruction and
                     # this one. Keep the position information of this instruction,
                     # but the operator and operand properties come from the other
