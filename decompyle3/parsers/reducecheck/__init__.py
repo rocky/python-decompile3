dec279dfdf0973c419708d9379b7f1dc235e767f
rocky <rocky@gnu.org>
2020-01-24 21:14:50 -0500

    Tidy grammar a little and ...
    add uncompyle 3.x tryexcept reduction rule which we may use later.

--- decompyle3/parsers/reducecheck/__init__.py
+++ decompyle3/parsers/reducecheck/__init__.py
@@ -6,5 +6,6 @@ from decompyle3.parsers.reducecheck.ifstmts_jump import *
 from decompyle3.parsers.reducecheck.or_check import *
 from decompyle3.parsers.reducecheck.testtrue import *
 from decompyle3.parsers.reducecheck.tryelsestmtl3 import *
+from decompyle3.parsers.reducecheck.tryexcept import *
 from decompyle3.parsers.reducecheck.while1elsestmt import *
 from decompyle3.parsers.reducecheck.while1stmt import *
