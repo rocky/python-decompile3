a6059bc179954a6a1f0b526f697dce6c11a48cca
rocky <rocky@gnu.org>
2020-02-03 20:29:42 -0500

    Reinstate a couple of tests

--- test/stdlib/3.7-exclude.sh
+++ test/stdlib/3.7-exclude.sh
@@ -1,7 +1,5 @@
 SKIP_TESTS=(
-    [test_httplib.py]=1 # FIXME: works on uncompyle6
     [test_itertools.py]=1 # FIXME: works on uncompyle6
-    [test_tcl.py]=1 # FIXME: works on uncompyle6
     [test_tempfile.py]=1 # FIXME: works on uncompyle6
     [test_zipimport.py]=1 # FIXME: works on uncompyle6
 
