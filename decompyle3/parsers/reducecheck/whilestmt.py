d4c8a1f2494ac44038099a7bfbfe398cc6bac72c
rocky <rocky@gnu.org>
2020-03-28 15:18:57 -0400

    COME_FROM-motivated change whilestmt reduce check
    In 3.7+ we are increasingly finding that that nesting
    of COME_FROM_LOOPs can appear in if/then's and other
    places.

--- decompyle3/parsers/reducecheck/whilestmt.py
+++ decompyle3/parsers/reducecheck/whilestmt.py
@@ -1,4 +1,16 @@
 #  Copyright (c) 2020 Rocky Bernstein
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 def whilestmt(
@@ -8,4 +20,12 @@ def whilestmt(
     # "while" statement is nested inside an if/else
     # so after the POP_BLOCK we have a JUMP_FORWARD which forms the "else" portion of the "if"
     # Check this.
-    return tokens[last-1] == "POP_BLOCK" and tokens[last] not in ("JUMP_FORWARD", "COME_FROM_LOOP")
+    # print("XXX", first, last, rule)
+    # for t in range(first, last): print(tokens[t])
+    # print("="*40)
+
+    return tokens[last - 1] == "POP_BLOCK" and tokens[last] not in (
+        "JUMP_FORWARD",
+        "COME_FROM_LOOP",
+        "COME_FROM",
+    )
