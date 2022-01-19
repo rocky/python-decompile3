3704331bc9dd018603d381cb0ecdb89b74e2dfb7
rocky <rb@dustyfeet.com>
2022-01-19 14:08:00 -0500

    Move generator expr rules to lambda_custom

--- decompyle3/parsers/p37/lambda_custom.py
+++ decompyle3/parsers/p37/lambda_custom.py
@@ -133,6 +133,42 @@ class Python37LambdaCustom(Python37BaseParser):
                 """
                 self.add_unique_doc_rules(rules_str, customize)
 
+            elif opname == "GET_AITER":
+                self.addRule(
+                    """
+                    expr                ::= generator_exp_async
+                    generator_exp_async ::= load_genexpr LOAD_STR MAKE_FUNCTION_0 expr
+                                            GET_AITER CALL_FUNCTION_1
+
+                    func_async_prefix   ::= _come_froms SETUP_EXCEPT GET_ANEXT LOAD_CONST YIELD_FROM
+                    func_async_middle   ::= POP_BLOCK JUMP_FORWARD COME_FROM_EXCEPT
+                                            DUP_TOP LOAD_GLOBAL COMPARE_OP POP_JUMP_IF_TRUE
+                                            END_FINALLY COME_FROM
+                    genexpr_func_async  ::= LOAD_FAST func_async_prefix
+                                            store func_async_middle comp_iter
+                                            JUMP_BACK COME_FROM
+                                            POP_TOP POP_TOP POP_TOP POP_EXCEPT POP_TOP
+
+                    expr                ::= list_comp_async
+                    list_comp_async     ::= LOAD_LISTCOMP LOAD_STR MAKE_FUNCTION_0
+                                            expr GET_AITER CALL_FUNCTION_1
+                                            GET_AWAITABLE LOAD_CONST
+                                            YIELD_FROM
+
+                    expr                ::= list_comp_async
+                    list_afor2          ::= func_async_prefix
+                                            store func_async_middle list_iter
+                                            JUMP_BACK COME_FROM
+                                            POP_TOP POP_TOP POP_TOP POP_EXCEPT POP_TOP
+                    list_comp_async     ::= BUILD_LIST_0 LOAD_FAST list_afor2
+                    get_aiter           ::= expr GET_AITER
+                    list_afor           ::= get_aiter list_afor2
+                    list_iter           ::= list_afor
+                   """,
+                    nop_func,
+                )
+                custom_ops_processed.add(opname)
+
             elif opname == "GET_AWAITABLE":
                 rule_str = """
                     await_expr ::= expr GET_AWAITABLE LOAD_CONST YIELD_FROM
