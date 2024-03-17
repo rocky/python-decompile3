667807ed7d062d0e94f49fb43bd1130e30da22e1
rocky <rb@dustyfeet.com>
2024-03-17 07:02:15 -0400

    Convert to using poetry TOML

--- setup.py
+++ setup.py
@@ -3,23 +3,7 @@
 
 import sys
 
-from __pkginfo__ import (
-    author,
-    author_email,
-    install_requires,
-    license,
-    long_description,
-    classifiers,
-    entry_points,
-    modname,
-    py_modules,
-    short_desc,
-    __version__,
-    web,
-    zip_safe,
-)
-
-import setuptools
+from setuptools import setup
 
 SYS_VERSION = sys.version_info[0:2]
 if SYS_VERSION < (3, 7):
@@ -28,21 +12,4 @@ if SYS_VERSION < (3, 7):
     print(mess)
     raise Exception(mess)
 
-setuptools.setup(
-    author=author,
-    author_email=author_email,
-    classifiers=classifiers,
-    description=short_desc,
-    entry_points=entry_points,
-    install_requires=install_requires,
-    license=license,
-    long_description=long_description,
-    name=modname,
-    packages=setuptools.find_packages(),
-    py_modules=py_modules,
-    test_suite="nose.collector",
-    url=web,
-    tests_require=["nose>=1.0"],
-    version=__version__,
-    zip_safe=zip_safe,
-)
+setup(packages=["decompyle3"])
