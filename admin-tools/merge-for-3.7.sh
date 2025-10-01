ec6b1cf4f29969a0295826d903d17913917b5670
rocky <rb@dustyfeet.com>
2025-10-01 05:47:49 -0400

    Packaging Administrivia

--- admin-tools/merge-for-3.7.sh
+++ admin-tools/merge-for-3.7.sh
@@ -1,7 +1,8 @@
 #/bin/bash
+PYTHON_VERSION=3.7
 decompyle3_merge_37_owd=$(pwd)
-(cd .. && PYTHON_VERSION=3.7 && pyenv local $PYTHON_VERSION)
 cd $(dirname ${BASH_SOURCE[0]})
+(cd .. && pyenv local $PYTHON_VERSION)
 if . ./setup-python-3.7.sh; then
     git merge master
 fi
